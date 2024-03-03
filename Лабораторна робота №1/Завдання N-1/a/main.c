27380b1887bb3499c8546179611d2630bb40154c
I-upgrade-myself <101337251+I-upgrade-myself@users.noreply.github.com>
2024-03-03 17:18:53 +0200

    main.c

--- Лабораторна робота №1/Завдання N-1/a/main.c
+++ Лабораторна робота №1/Завдання N-1/a/main.c
@@ -1,30 +1,31 @@
-#include stdio.h
+#include <stdio.h>
 
 int main() {
     int num1, num2, num3;
 
-   //  Введення чисел користувачем
-    printf(Введіть перше число );
-    scanf(%d, &num1);
+    // Введення чисел користувачем
+    printf("Введіть перше число: ");
+    scanf("%d", &num1);
 
-    printf(Введіть друге число );
-    scanf(%d, &num2);
+    printf("Введіть друге число: ");
+    scanf("%d", &num2);
 
-    printf(Введіть третє число );
-    scanf(%d, &num3);
+    printf("Введіть третє число: ");
+    scanf("%d", &num3);
 
-     if (num1 > num2 && num1 > num3) {
-        printf(Максимальне число %dn, num1);
+    if (num1 > num2 && num1 > num3) {
+        printf("Максимальне число: %d\n", num1);
     } 
     else if (num2 > num1 && num2 > num3) {
-        printf(Максимальне число %dn, num2);
+        printf("Максимальне число: %d\n", num2);
     } 
     else if (num3 > num1 && num3 > num2) {
-        printf(Максимальне число %dn, num3);
+        printf("Максимальне число: %d\n", num3);
     } 
     else {
-        printf(Всі числа рівніn);
+        printf("Всі числа рівні\n");
     }
 
     return 0;
 }
+
